24aa2aaca0c9a22ea7df3aef48e8d5de8278065f
hirose <yukihio76@gmail.com>
2026-07-18 12:27:48 +0900

    読み取り時にローディングの追加

--- src/screens/ScanScreen.tsx
+++ src/screens/ScanScreen.tsx
@@ -14,6 +14,7 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../App";
 import { useApi } from "../hooks/useApi";
+import LoadingOverlay from "../components/Loadingoverlay";
 
 type Nav = StackNavigationProp<RootStackParamList>;
 
@@ -170,6 +171,7 @@ export default function ScanScreen() {
           </View>
         </View>
       </View>
+      <LoadingOverlay visible={scanned} message="読み取り中..." />
     </View>
   );
 }
